ed87aa816331a2f4afe42519720b4c578c4c8dfc
Justigo <justinlevk@gmail.com>
2019-03-15 20:53:03 -0700

    test2

--- Dating_Sim/src/GameDriver.java
+++ Dating_Sim/src/GameDriver.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class GameDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		Scanner input = new Scanner(System.in);
+		
 	}
 
 }
