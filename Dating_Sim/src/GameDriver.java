934abe7d8450cada5d385e1a93ab0a31128d2b9a
Justigo <justinlevk@gmail.com>
2019-03-15 21:20:38 -0700

    9:19 PM 3/15

--- Dating_Sim/src/GameDriver.java
+++ Dating_Sim/src/GameDriver.java
@@ -2,10 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class GameDriver {
-	ArrayList<File> name = ("apple.txt", "e.txt");
+	private ArrayList<File> ChartextFiles = new ArrayList<File> (Arrays.asList() );
+	private Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
 		System.out.println("After a long week of school, you stayed up thinking about life and its complexities. When you arise, "
 				+ "you have only one thought in your head, 'It's MATING SEASON!");
 		
