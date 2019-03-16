94ce88f275751afb791705fda0ec4c2d05cb8786
Justigo <justinlevk@gmail.com>
2019-03-15 21:07:17 -0700

    added an arraylist for all character text files

--- Dating_Sim/src/GameDriver.java
+++ Dating_Sim/src/GameDriver.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class GameDriver {
-
+	ArrayList<File> name = ("apple.txt", "e.txt");
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
+		System.out.println("After a long week of school, you stayed up thinking about life and its complexities. When you arise, "
+				+ "you have only one thought in your head, 'It's MATING SEASON!");
 		
 	}
 
