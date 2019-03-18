c0e55b48790e389f8d8a5b5332a51702048cb38b
jlim128 <38666100+jlim128@users.noreply.github.com>
2019-03-18 12:45:47 -0700

    Update GameDriver.java

--- Dating_Sim/src/GameDriver.java
+++ Dating_Sim/src/GameDriver.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class GameDriver {
 	private ArrayList<String> ChartextFiles = new ArrayList<String> ();
 	private Scanner input = new Scanner(System.in);
+	//hi
 	
 	public static void main(String[] args) {
 		System.out.println("After a long week of school, you stayed up thinking about life and its complexities. When you arise, "
