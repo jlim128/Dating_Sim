cabc5064f67e9d40061b0a3da5837440e63f4716
Justigo <38697824+Justigo@users.noreply.github.com>
2019-03-25 23:24:59 -0700

    Runs till character story

--- Dating_Sim/src/GameDriver.java
+++ Dating_Sim/src/GameDriver.java
@@ -2,13 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class GameDriver {
-	private ArrayList<String> ChartextFiles = new ArrayList<String> ();
 	private Scanner input = new Scanner(System.in);
-	//hi
+	private ArrayList<Character> Characters = new ArrayList<Character> ();
+	private ArrayList<String> ChartextFiles = new ArrayList<String> (Arrays.asList("aristotle.txt", "friere.txt", "hume.txt", 
+										       "kant.txt", "schopenhauer.txt", "singer.txt"));
 	
 	public static void main(String[] args) {
 		System.out.println("After a long week of school, you stayed up thinking about life and its complexities. When you arise, "
 				+ "you have only one thought in your head, 'It's MATING SEASON!");
+		System.out.println("\n\n\n\n\n\n============PICK A LOCATION #NUMBER============");
+		for(x =0;x < Characters.size();x++){
+			System.println((x +1) + ") " + Characters.get(x).getLocation());
+		}
+		int response = input.nextInt();
 		
 	}
 
